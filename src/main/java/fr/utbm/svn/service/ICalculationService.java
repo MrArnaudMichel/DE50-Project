673ffaa9733294eb9600cdd2a67f67459f9b9b60
gretter6 <guillaume.retter@utbm.fr>
2026-06-12 19:09:13 +0200

    fix: diagram was null when changing arc values, add method to get the activ diagram

--- src/main/java/fr/utbm/svn/service/ICalculationService.java
+++ src/main/java/fr/utbm/svn/service/ICalculationService.java
@@ -1,8 +1,9 @@
 package fr.utbm.svn.service;
 
 import com.telelogic.rhapsody.core.IRPApplication;
+import com.telelogic.rhapsody.core.IRPDiagram;
 import com.telelogic.rhapsody.core.IRPProject;
 
 public interface ICalculationService {
-    void calculateImportance(IRPProject project, IRPApplication app);
+    void calculateImportance(IRPProject project, IRPDiagram diagram);
 }
